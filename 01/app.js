adc0b46a50c3317eb467fbc23accb4d6df00db03
songminhyeok <55730465+songminhyeok@users.noreply.github.com>
2020-03-28 00:08:10 +0900

    01수업과제

---
+++ 01/app.js
@@ -0,0 +1,22 @@
+console.log('app.js');
+
+var age = 20;
+
+console.log(age);
+
+var abc = true;
+
+console.log(abc);
+
+var month = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+console.log(month);
+
+var image ={
+    width : 100,
+    height : 100,
+    name: '1.png'
+};
+
+
+console.log(image);
