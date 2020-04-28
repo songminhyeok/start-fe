22d964c2c827273565b6c6d6aa6fcc44904832eb
songminhyeok <songminhyeok@naver.com>
2020-04-28 22:48:30 +0900

    수정

--- 01/app.js
+++ 01/app.js
@@ -1,22 +1,89 @@
+
 console.log('app.js');
 
-var age = 20;
 
-console.log(age);
+//변수는 var 사용 권장
+var name = '김지연';
+var Name = '김지연';
 
-var abc = true;
 
-console.log(abc);
+console.log(name);
+console.log(Name);
 
-var month = [1,2,3,4,5,6,7,8,9,10,11,12];
 
-console.log(month);
+//주석
+console.log(1);
+console.log(2);
 
-var image ={
-    width : 100,
-    height : 100,
-    name: '1.png'
+/*
+
+*/
+
+
+// 숫자형
+var count = 1;
+console.log(count);
+
+
+var isMan = true;  //bool
+var isMan = "true"; //String
+
+console.log(isMan);
+
+
+//함수도 데이터 타입이다
+function log(str) {
+    console.log(str);
 };
 
 
-console.log(image);
+var log = function(str) {
+    console.log(str);
+};
+
+
+//객체
+var image = {
+    width: 100,
+    height: 100,
+    name:'1.png'
+};
+
+var image1 = [1,2,3,4,5,6,'a',{}];
+image1[0] = 'a';
+
+
+var date = null; // 값이 없음
+var data1 = '';
+var date2; //undefined
+
+console.log(data, data1, data2);
+
+// 이렇게 쓸수도 있는데 복잡함
+
+var imageWidth = 100;
+var imageHeight = 100;
+console.log(image, image1);
+
+
+console.log(1+2); //3
+console.log('a' + 'b'); //ab
+console.log(1+'4'); //14
+
+
+var count = 1;
+var number ='2';
+
+
+console.log(count + number); // 12
+console.log(1 == 1); // 동등, true
+console.log(1 == 2); // fales
+console.log('' == '0'); // fales
+console.log(0 == '') // true, 값들이 자동으로 bool형변환이 되서 fales = fales
+
+var isHero = true;
+if (isHero && isHulk) {
+    console.log('hero')
+}
+
+console.log(typeof(1)); // 어떤 타입인지 알려줌
--- 02/app.js
+++ 02/app.js
@@ -1,20 +1,17 @@
 console.log('app');
 
-/*
-// Create
-var todos = ['운동'];
-var todo ='게임';
+// create
+var todos = ['운동']
+var todo = '게임';
 todos.push(todo);
 console.log(todos);
 
-//read
-
-todos.forEach(function(todo) {
-
-    console.log(todo);
+// Read
+todos.forEach(function(todo){
+    console.log()
 });
-/*
-// update
+
+// Update 
 var todos = ['운동', '게임'];
 var updateTodo = '게임';
 var updateIndex = todos.findIndex(function(todo) {
@@ -22,92 +19,74 @@ var updateIndex = todos.findIndex(function(todo) {
 });
 todos[updateIndex] = '공부';
 console.log(todos);
-*/
 
-/*var todos = ['운동', '공부', '목욕'];
+// Delete 1
+var todos = ['운동', '공부', '목욕'];
 var deleteTodo = '공부';
 var deleteIndex = todos.findIndex(function(todo) {
   return todo === deleteTodo;
 });
 todos.splice(deleteIndex, 1);
 console.log(todos);
-*/
 
-/*var isMan = true;
+// if
+var isMan = false;
 if(isMan) {
     console.log('1');
 } 
 else if(!isMan && isHero){
-     
+
 }
-else{
+else {
     console.log('2');
 }
-*/
 
-//window.alert('1');
-//alert('안녕하세요');
+alert('안녕하세요');
+var who = propmpt('당신은 누구세요?');
+console.log(who);
 
-//var who = prompt('당신은 누구세요?:');
-//console.log(who);
+var isDelete = confirm('정말 삭제하시겠습니까?'); 
+conlose.log(isDelete);
 
-//var isDelete = confirm('정말 삭제하시겠습니까?');
 
-//console.log(isDelete);
+parseInt('12'); 
 
-/////////배열 count 요소들의 평균값 구하는 코드를 작성하세요/////
-/*
-function average(array) {
-    var sum = 0.0;
-  
-    for (var i = 0; i < array.length; i++)
-      sum += array[i];
-  
-    return sum / array.length;
+var id = setTimeout(test, 1000); 
+var id = setInterval(test, 1000); 
 
+var nick = 'aji'; 
+
+function test() {
+  var age = 30; 
+  console.log(nick, age);
 }
 
-var count = [10,20,30,40,50,60,70,100]; 
-console.log("배열의 평균값 : " + average(count))
+test();
+console.log(age); 
 
-*/
-
-
-/////////////구구단을 출력하는 코드를 for구문을 사용하여 작성하세요 ////
-///구구단코드를 함수로 작성하세요 /////
-
-/*
-
-function gugudan(i){
-for(var i =a; i<10; i++) {
-    for(var j = 1; j < 10; j++) {
-        console.log(i + "*" + j + "=" + i * j);
-         }
-    }
-}
-
-var a = 2;
-console.log(gugudan(a));
-*/
-
-
-////////메소드가 포함된 객체를 생성하세요 ////
-/*
 function sum(a, b) {
-    return a + b;
-  }
-  sum(1, 3);
-*/
-
-var result = 0;
-var i = 1;
-while (true) {
-    var value = window.prompt("더할 숫자를 입력해주세요!" + " ");
-    if (value == null) {
-        alert("숫자의 총 합은:" + result);
-        break;
-    }
-
-    result += parseInt(value);
-    
+  return a + b;
 }
+console.log(sum(1, 3));
+
+var car = {
+  year: '2014',
+  starting: function() {
+    console.log('starting..');
+  },
+};
+
+car.starting();
+
+(function() {
+    var a = 'a';
+    console.log
+})(); 
+
+function sum(x, y) {
+  if (!y) y = 1;
+  console.log(x + y);
+}
+console.log(sum(3)); 
+
+
